cf6af796f46d07a00986645dd5c81580d1f2ee8d
jefry <jefrycruz88@gmail.com>
2021-08-01 20:13:19 -0400

    added Frirestore to the project

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,35 @@
 import React, {useState} from 'react';
 import './App.css';
+// Firebase App (the core Firebase SDK) is always required and must be listed first
+import firebase from "firebase/app";
+// If you are using v7 or any earlier version of the JS SDK, you should import firebase using namespace import
+// import * as firebase from "firebase/app"
+
+// If you enabled Analytics in your project, add the Firebase SDK for Analytics
+import "firebase/analytics";
+
+// Add the Firebase products that you want to use
+// import "firebase/auth";
+import "firebase/firestore";
+
+
+
+
+// IMPORT ABOVE
+//========================================================================================
+var firebaseConfig = {
+  apiKey: "AIzaSyCxz8wAWw11M1BfBFhywGmduzf9sJmhiGc",
+  authDomain: "simple-polls-abbf0.firebaseapp.com",
+  projectId: "simple-polls-abbf0",
+  storageBucket: "simple-polls-abbf0.appspot.com",
+  messagingSenderId: "36007211130",
+  appId: "1:36007211130:web:17d059c3b95471f92ccbc3",
+  measurementId: "G-9SDE7ZW4M3"
+};
+firebase.initializeApp(firebaseConfig);
+const db = firebase.firestore()
+// LIBRARY CONFIGS ABOVE
+//========================================================================================
 
 type Option = {
   optionIndex: number,
@@ -57,6 +87,17 @@ function App(this: any) {
   // const submitVote = (optionIndex: number) => {
   //   console.log("HELLO "+ optionIndex)
   // }
+
+
+  // this Firebase Firestore insert call works!
+  // db.collection("users").add({
+  //   firstName: "Jefry",
+  //   lastName: "Cruz",
+  //   email: "test@test.com"
+  // })
+  // .then((docRef) => {
+  //     console.log("Document written with ID: ", docRef.id);
+  // })
 
   const poll: Poll = getPoll()
   const options = getOptions()
